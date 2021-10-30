6747a274ee0821b91fcd74b9c9d1769f8652d7e8
mvBellArrington <90705087+mvBellArrington@users.noreply.github.com>
2021-10-30 00:05:59 -0400

    Student config update
    Replaced given fields to reflect my personal information

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Tonya A. Melvin-Bryant', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Mone Bell-Arrington', // Title for your website.
+    tagline: 'Forever ambitious in learning new technologies while upholding passion for Full Stacker future career',
     profilePhoto: 'img/profile.png',
-    url: 'https://tamelvinbryant.github.io', // Your website URL
+    url: 'https://mvBellArrington.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'tamelvinbryant',
+    organizationName: 'mvBellArrington',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: 'tonya-melvin-bryant-ab92b855',
-        twitter: 'tonyamccu',
-        github: 'tamelvinbryant',
+        linkedin: 'mone-bellarrington',
+        facebook: 'Mone Bell-Arrington',
+        github: 'mvBellArrington',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
